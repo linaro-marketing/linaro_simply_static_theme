a3e3cd67d28bdf5e7b931ef46d0408f6adf183ce
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-10-09 12:23:36 +0100

    Added JS to hide the universal nav

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -1,2 +1,17 @@
-// Websites main js file
-Foundation.addToJquery($);
+var universal_nav_toggled = false;
+$('#universal_nav_toggle').on('click', function(){
+    universal_nav_toggled = !universal_nav_toggled;
+});
+
+$(document).on("scroll", function () {
+    if ($(document).scrollTop() > 0) {
+        $("#universal_nav_sticky").hide();
+        if (universal_nav_toggled === true) {
+            $('#universal_nav_toggle').trigger('click');
+        }
+    }
+    if ($(document).scrollTop() == 0) {
+        universal_nav_toggled = false;
+        $("#universal_nav_sticky").slideDown("fast");
+    }
+});
